6690501425122f6d74321c3708aa0ed6ea126d1d
ritchie <ritchieg9@gmail.com>
2020-05-18 12:21:14 -0500

    Example working for Movie Search

--- app/src/main/java/com/cocoa/mediaplayer/VideoProvider.kt
+++ app/src/main/java/com/cocoa/mediaplayer/VideoProvider.kt
@@ -8,6 +8,7 @@ import android.provider.MediaStore
 import android.util.Log
 import androidx.annotation.RequiresApi
 import com.cocoa.mediaplayer.api.MovieDetails
+import com.cocoa.mediaplayer.api.MovieResults
 import com.cocoa.mediaplayer.api.TheMovieDBClient
 import com.cocoa.mediaplayer.api.TheMovieDBInterface
 import retrofit2.Call
@@ -39,22 +40,41 @@ object VideoProvider : Activity() {
         val rs: Cursor? = activity.applicationContext.getContentResolver().query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, proj, null, null, orderBy)
 
         // MAKE CALL WITH ID
-        val request = TheMovieDBClient.ServiceBuilder.buildService(TheMovieDBInterface::class.java)
-        val call = request.getMovieDetails(800, API_KEY)
+//        val request = TheMovieDBClient.ServiceBuilder.buildService(TheMovieDBInterface::class.java)
+//        val call = request.getMovieDetails(800, API_KEY)
+//
+//        Log.d("VIDEO","MAAAAAAAAUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU")
+//
+//        call.enqueue(object : Callback<MovieDetails>{
+//            override fun onResponse(call: Call<MovieDetails>, response: Response<MovieDetails>) {
+//                if (response.isSuccessful){
+//                    Log.d("VIDEO","ALGO MUY BUENO PASO")
+//                    Log.d("VIDEO",response.body().toString())
+//                }
+//            }
+//            override fun onFailure(call: Call<MovieDetails>, t: Throwable) {
+//                Log.d("VIDEO","ALGO MUY MALO PASO")
+//            }
+//        })
+        // MEND OF CALL
 
-        Log.d("VIDEO","MAAAAAAAAUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU")
-
-        call.enqueue(object : Callback<MovieDetails>{
-            override fun onResponse(call: Call<MovieDetails>, response: Response<MovieDetails>) {
-                if (response.isSuccessful){
-                    Log.d("VIDEO","ALGO MUY BUENO PASO")
-                    Log.d("VIDEO",response.body().toString())
-                }
-            }
-            override fun onFailure(call: Call<MovieDetails>, t: Throwable) {
-                Log.d("VIDEO","ALGO MUY MALO PASO")
-            }
-        })
+        // MAKE CALL Search
+//        val request = TheMovieDBClient.ServiceBuilder.buildService(TheMovieDBInterface::class.java)
+//        val call = request.searchMovie(API_KEY,"en-US", "placebo", "2015" )
+//
+//        Log.d("VIDEO","MAAAAAAAAUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU")
+//
+//        call.enqueue(object : Callback<MovieResults>{
+//            override fun onResponse(call: Call<MovieResults>, response: Response<MovieResults>) {
+//                if (response.isSuccessful){
+//                    Log.d("VIDEO","ALGO MUY BUENO PASO")
+//                    Log.d("VIDEO",response.body().toString())
+//                }
+//            }
+//            override fun onFailure(call: Call<MovieResults>, t: Throwable) {
+//                Log.d("VIDEO","ALGO MUY MALO PASO")
+//            }
+//        })
         // MEND OF CALL
 
 
--- app/src/main/java/com/cocoa/mediaplayer/api/TheMovieDBInterface.kt
+++ app/src/main/java/com/cocoa/mediaplayer/api/TheMovieDBInterface.kt
@@ -13,5 +13,5 @@ interface TheMovieDBInterface {
 
     //https://api.themoviedb.org/3/search/movie?api_key=a6ea5bd224b1d6ca252e6209cca8b9d7&language=en-US&query=placebo&year=2015
     @GET("search/movie")
-    fun searchMovie(@Query("api_key") key: String, @Query("language") language: String, @Query("query") query: String): Call<MovieResults>
+    fun searchMovie(@Query("api_key") key: String, @Query("language") language: String, @Query("query") query: String, @Query("year") year: String): Call<MovieResults>
 }
